52a40e7dcff59377028b67dd4e627cb4040f20ef
Anton Grakhov <ant.grakhov@gmail.com>
2019-11-04 02:13:12 +0500

    Change development to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path')
 
 module.exports = {
-	mode: 'development',
+	mode: 'production',
 	entry: './assets/js/document-setting-panels.js',
 	output: {
 		filename: 'document-setting-panels.build.js',
